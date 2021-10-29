6087f0a548dfa382939f3f6b4d4af95e2471601e
Eberthyn Marques <eberthyn@gmail.com>
2021-10-29 18:02:21 -0300

    Ajustando Compatibilidade do Projeto (CLI/Node)

---
+++ alurapic/src/app/shared/components/loading/loading.service.ts
@@ -0,0 +1,27 @@
+import { LoadingType } from "./loading-type";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
+import { startWith } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class LoadingService {
+  loadingSubject = new Subject<LoadingType>();
+
+  getLoading() {
+    return this.loadingSubject
+      .asObservable()
+      .pipe(startWith(LoadingType.STOPPED));
+  }
+
+  start() {
+    this.loadingSubject.next(LoadingType.LOADING);
+  }
+
+  stop() {
+    this.loadingSubject.next(LoadingType.STOPPED);
+  }
+
+  constructor() {}
+}
