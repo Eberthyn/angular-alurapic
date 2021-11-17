7a3c060c15cc6492e0e5339a061e992e90838074
Eberthyn Marques <eberthyn@gmail.com>
2021-11-17 15:21:43 -0300

    Enviando LOG de error para o BackEnd

--- alurapic/src/app/errors/errors.module.ts
+++ alurapic/src/app/errors/errors.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
+import { GlobalErrorHandler } from './global-error-handler/global-error-handler';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
   ],
-  declarations: [NotFoundComponent]
+  declarations: [NotFoundComponent],
+  providers: [{
+    provide: ErrorHandler,
+    useClass: GlobalErrorHandler
+  }]
 })
 export class ErrorsModule { }
